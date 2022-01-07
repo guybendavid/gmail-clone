b6929405e815a5d9b9eda0180864a82b4faf27ae
Guy <guybdd123@gmail.com>
2022-01-07 20:08:46 +0200

    Unauthenticated bug fix

--- client/src/contexts/AppContext.tsx
+++ client/src/contexts/AppContext.tsx
@@ -2,7 +2,6 @@ import { createContext, ReactNode } from "react";
 import { Store, useStore } from "store/store";
 import { ApolloError } from "@apollo/client";
 import { useMediaQuery } from "@material-ui/core";
-import { getAuthData } from "services/auth";
 
 export type AppContextType = {
   isSmallScreen: boolean;
@@ -17,7 +16,6 @@ interface Props {
 const AppContext = createContext<AppContextType | undefined>(undefined);
 
 const AppContextProvider = ({ children }: Props) => {
-  const { isAuthenticated } = getAuthData();
   const setIsComposeOpened = useStore((state: Store) => state.setIsComposeOpened);
   const setSnackBarMessage = useStore((state: Store) => state.setSnackBarMessage);
   const isSmallScreen = useMediaQuery("(max-width:765px)");
@@ -30,12 +28,11 @@ const AppContextProvider = ({ children }: Props) => {
 
   const handleErrors = (error: any) => {
     const { message: gqlErrorMessage } = error;
-    let content = null;
+    let content;
 
-    const isAuthForm = ["/login", "/register"].includes(window.location.pathname);
     const gqlContextErrorMessage = error.networkError?.result?.errors[0]?.message?.split("Context creation failed: ")[1];
 
-    if (gqlContextErrorMessage === "Unauthenticated" || (!isAuthenticated && !isAuthForm)) {
+    if (gqlContextErrorMessage === "Unauthenticated") {
       logout();
       content = gqlContextErrorMessage || "Unauthenticated";
     } else if (gqlContextErrorMessage) {
