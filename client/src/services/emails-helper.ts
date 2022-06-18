9573dca61cd7565f5e2cb0056332b8eff9fa28e3
Guy <guybdd123@gmail.com>
2022-06-18 16:00:26 +0300

    function positions change

--- client/src/services/emails-helper.ts
+++ client/src/services/emails-helper.ts
@@ -19,36 +19,6 @@ interface UpdateCachedEmailsListOptions extends PrevDataOptions {
   newEmail: Email;
 }
 
-function getQueryOptions(query: DocumentNode, loggedInUserEmail: string) {
-  return { query, variables: { loggedInUserEmail } };
-};
-
-function getPrevData({ query, loggedInUserEmail, client }: PrevDataOptions): Email[] {
-  const queryOptions = getQueryOptions(query, loggedInUserEmail);
-  const { getReceivedEmails, getSentEmails } = client.readQuery(queryOptions);
-  return getReceivedEmails ? getReceivedEmails : getSentEmails;
-};
-
-function writeToCache(client: ApolloClient<any>, queryOptions: QueryOptions, isReceivedEmails: boolean, newData: Email[]) {
-  const objectToCache = {
-    ...queryOptions,
-    data: isReceivedEmails ? { getReceivedEmails: newData } : { getSentEmails: newData }
-  };
-
-  client.writeQuery(objectToCache);
-};
-
-function updateCachedEmailsList({ query, loggedInUserEmail, client, newEmail }: UpdateCachedEmailsListOptions) {
-  const queryOptions = getQueryOptions(query, loggedInUserEmail);
-
-  if (client.readQuery(queryOptions)) {
-    const prevData = getPrevData({ query, loggedInUserEmail, client });
-    const newData = [newEmail, ...prevData];
-    const isReceivedEmails = query === GET_RECEIVED_EMAILS;
-    writeToCache(client, queryOptions, isReceivedEmails, newData);
-  }
-};
-
 export function addNewEmailToCache(newEmail: Email, loggedInUserEmail: string, client: ApolloClient<any>) {
   const { email: recipientEmail } = newEmail.recipient;
   const { email: senderEmail } = newEmail.sender;
@@ -71,4 +41,34 @@ export function deleteEmailsFromCache(idsToDelete: string[], activeTab: number, 
   const newData = Object.values(prevData).filter(email => !idsToDelete.includes(email.id));
   const queryOptions = getQueryOptions(query, loggedInUserEmail);
   writeToCache(client, queryOptions, isReceivedEmails, newData);
+};
+
+function updateCachedEmailsList({ query, loggedInUserEmail, client, newEmail }: UpdateCachedEmailsListOptions) {
+  const queryOptions = getQueryOptions(query, loggedInUserEmail);
+
+  if (client.readQuery(queryOptions)) {
+    const prevData = getPrevData({ query, loggedInUserEmail, client });
+    const newData = [newEmail, ...prevData];
+    const isReceivedEmails = query === GET_RECEIVED_EMAILS;
+    writeToCache(client, queryOptions, isReceivedEmails, newData);
+  }
+};
+
+function getPrevData({ query, loggedInUserEmail, client }: PrevDataOptions): Email[] {
+  const queryOptions = getQueryOptions(query, loggedInUserEmail);
+  const { getReceivedEmails, getSentEmails } = client.readQuery(queryOptions);
+  return getReceivedEmails ? getReceivedEmails : getSentEmails;
+};
+
+function getQueryOptions(query: DocumentNode, loggedInUserEmail: string) {
+  return { query, variables: { loggedInUserEmail } };
+};
+
+function writeToCache(client: ApolloClient<any>, queryOptions: QueryOptions, isReceivedEmails: boolean, newData: Email[]) {
+  const objectToCache = {
+    ...queryOptions,
+    data: isReceivedEmails ? { getReceivedEmails: newData } : { getSentEmails: newData }
+  };
+
+  client.writeQuery(objectToCache);
 };
